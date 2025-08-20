8011817b2cb2dc2d6dc4f0f7204415bf4633cd85
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-08-20 11:21:52 +0530

    Updated the sign in route in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-import { Header } from "@/components/Landing_page/Header";
-import { Hero } from "@/components/Landing_page/Hero";
-import { Features } from "@/components/Landing_page/Features";
-import { Footer } from "@/components/Landing_page/Footer";
 import React from "react"
 import { Routes, Route} from 'react-router-dom';
 import { LandingPage } from "./components/Landing_page/LandingPage";
 import { SignUpPage } from "./components/auth/Signup";
+import { SignIn} from "./components/auth/SignIn"
 function App() {
    return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-    <Header />
-    <Hero />
-    <Features />
-    <Footer />
-    </div>
     // routes consist of all the paths or address to be made for navigation
     <Routes> 
       {/* Route is used to define path for a page
       Source: https://www.w3schools.com/react/react_router.asp  */}
       <Route path="/" element={<LandingPage />} /> 
       <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/login" element={<SignIn />} />
+      
     </Routes>
   )
 }
